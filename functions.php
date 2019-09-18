7532a443a9cb0801bcf3b65690dcadc5d7a8fc72
gumarov1991 <gumarov2017@yandex.ru>
2019-09-18 21:52:05 +0600

    beginning

--- functions.php
+++ functions.php
@@ -1,1 +1,54 @@
 <?php
+
+add_action( 'wp_enqueue_scripts', 'bs_stels_48_media' );
+add_action( 'after_setup_theme', 'bs_stels_48_after_setup' );
+add_action( 'widgets_init', 'test_register_my_widgets' );
+add_theme_support( 'post-thumbnails' );
+
+
+
+function bs_stels_48_media()
+{
+    wp_enqueue_style( 'style', get_template_directory_uri() . '/css/normalize.css' );
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
+    wp_enqueue_style( 'style', get_template_directory_uri() . '/css/media.css' );
+    wp_enqueue_style( 'style', get_template_directory_uri() . '/libs/flex_css/flex.css' );
+    wp_enqueue_style( 'style', get_template_directory_uri() . '/libs/fancybox/jquery.fancybox.min.css' );
+    wp_enqueue_style( 'style', get_template_directory_uri() . '/libs/owlcarousel/owl.carousel.min.css' );
+    wp_enqueue_style( 'style', 'https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700&display=swap' );
+    wp_enqueue_script( 'script', '//code.jquery.com/jquery-3.3.1.min.js');
+    wp_enqueue_script( 'script', get_template_directory_uri() . '/libs/fancybox/jquery.fancybox.min.js');
+    wp_enqueue_script( 'script', get_template_directory_uri() . '/libs/owlcarousel/owl.carousel.min.js');
+    wp_enqueue_script( 'script', 'https://api-maps.yandex.ru/2.1/?apikey=<2044817c-a630-48b1-acf8-5e08fd4884d3>&lang=ru_RU');
+    wp_enqueue_script( 'script', 'https://kit.fontawesome.com/d65132e2be.js');
+    wp_enqueue_script( 'script', get_template_directory_uri() . '/js/common.js');
+}
+
+function bs_stels_48_after_setup()
+{
+    register_nav_menu('top-menu', 'Верхнее меню');
+    register_nav_menu('footer-menu', 'Меню футера');
+}
+
+/*function test_register_my_widgets()
+{
+    register_sidebar([
+        'name' => "Правая боковая панель сайта",
+        'id' => 'right-sidebar',
+        'before_widget' => '',
+        'after_widget' => '',
+        'description' => 'Эти виджеты будут показаны в правой колонке сайта',
+        'before_title' => '<h2>',
+        'after_title' => '</h2>'
+    ]);
+
+    register_sidebar([
+        'name' => "Верхняя панель сайта",
+        'id' => 'top-sidebar',
+        'before_widget' => '',
+        'after_widget' => '',
+        'description' => 'Эти виджеты будут в верхней части сатйта',
+        'before_title' => '<h2>',
+        'after_title' => '</h2>'
+    ]);
+}*/
--- header.php
+++ header.php
@@ -6,22 +6,9 @@
     <title><?php get_bloginfo('name')?></title>
     <meta name="description" content="<?php bloginfo('description') ?>">
     <meta name="author" content="Breakout Studio">
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="style.css?v=1.0.0">
-    <link rel="stylesheet" href="css/media.css?v=1.0.0">
-    <link rel="stylesheet" href="libs/flex_css/flex.css">
-    <link rel="stylesheet" href="libs/fancybox/jquery.fancybox.min.css">
-    <link rel="stylesheet" href="libs/owlcarousel/owl.carousel.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700&display=swap" rel="stylesheet">
-    <script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
-    <script src="libs/fancybox/jquery.fancybox.min.js"></script>
-    <script src="libs/owlcarousel/owl.carousel.min.js"></script>
-    <script src="https://api-maps.yandex.ru/2.1/?apikey=<2044817c-a630-48b1-acf8-5e08fd4884d3>&lang=ru_RU" type="text/javascript"></script>
-    <script src="https://kit.fontawesome.com/d65132e2be.js"></script>
     <?php wp_head(); ?>
 </head>
 <body>
-<script src="js/common.js"></script>
 <header class="flex_main flex_jcontent-between">
     <div class="logo flex_main flex_column flex_jcontent-center">
         <p>stels48</p>
